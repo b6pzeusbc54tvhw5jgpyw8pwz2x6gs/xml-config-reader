880f5380f029a93a94139c54022f61fbeba07dc2
Alfred UC <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2018-10-20 18:49:34 +0900

    refactor: apply xmldoc

--- src/core.js
+++ src/core.js
@@ -1,8 +1,6 @@
-const LineByLineReader = require('line-by-line')
-const decomment = require('decomment');
 const fs = require('fs')
 const path = require('path')
-const temp = require("temp");
+const xmldoc = require('xmldoc')
 
 module.exports = (file, key) => new Promise( (resolve,reject) => {
 
@@ -10,104 +8,21 @@ module.exports = (file, key) => new Promise( (resolve,reject) => {
     file = path.resolve( process.cwd(), file )
   }
 
-  let fullText
   try {
-    fullText = fs.readFileSync(file, 'utf8')
+    const fullText = fs.readFileSync(file, 'utf8')
+    const document = new xmldoc.XmlDocument(fullText)
+    const found = document.descendantWithPath(key)
+    if( found ) {
+      console.log(found)
+      resolve({ line: found.line, value: found.val })
+    } else {
+      console.log(found)
+      reject(new Error('NOT_FOUND'))
+    }
+
   } catch( e ) {
     console.log(e)
     process.exit(1)
   }
-
-  fullText = decomment.html(fullText, { space: true })
-  const tmpName = temp.path({suffix: '.tmp'});
-  fs.writeFileSync(tmpName, fullText, 'utf8')
-
-  const splited = key.split('.')
-
-  // a.b.c 에서 현재 a 뎁스를 찾고있으면 0
-  let findingDepth = 0
-
-  // <a><b> 이후 탐색 하고 있을때의 뎁스: 2
-  let currentDepth = 0
-  let currentLine = 0
-
-  const tagOpenMatcher = /^\s*?<([a-zA-Z0-9][a-zA-Z0-9-_]*?)>/
-  const valueWithTagCloseMatcher = /^(.+?)<\/([a-zA-Z0-9][a-zA-Z0-9-_]*?)>/
-  const valueMatcher = /(.*)/
-  const tagCloseMatcher = /^\s*?<\/([a-zA-Z0-9][a-zA-Z0-9-_]*?)>/
-  let collectingValue = ''
-
-  const lr = new LineByLineReader(tmpName)
-  lr.on('line', (line) => {
-
-    if( currentLine > 190 ) debugger
-    currentLine++
-
-    let waitValue = false
-    let matched
-
-    while(true) {
-
-      if( ! line.trim() ) break
-
-      matched = line.match(tagOpenMatcher)
-      if( matched ) {
-        line = line.slice(matched[0].length)
-        if( matched[1] === 'config-root') continue;
-
-        if( currentDepth === findingDepth && splited[findingDepth] === matched[1]) {
-          findingDepth++
-        }
-
-        currentDepth++
-        continue
-      }
-
-      matched = line.match(tagCloseMatcher)
-      if( matched) {
-        line = line.slice(matched[0].length)
-        if( collectingValue ) {
-          resolve({ line: currentLine, value: collectingValue })
-          lr.close()
-          break
-        }
-        currentDepth--
-        continue
-      }
-
-      matched = line.match(valueWithTagCloseMatcher)
-      if( matched) {
-        line = line.slice(matched[0].length)
-        if( splited.length === findingDepth) {
-          if( matched[2] !== splited[findingDepth-1]) {
-            console.error('wrong config. line: ' + currentLine)
-            process.exit(1)
-          }
-          collectingValue = matched[1]
-          resolve({ line: currentLine, value: collectingValue })
-          lr.close()
-          break
-        }
-
-        currentDepth--
-        continue
-      }
-
-      matched = line.match(valueMatcher)
-      if( matched) {
-        line = line.slice(matched[1].length)
-        if( splited.length === findingDepth) {
-          collectingValue += matched[1]
-        }
-        continue
-      }
-    }
-
-  }).on('end', () => {
-    if( ! collectingValue ) {
-      console.log('not found by key: ' + key)
-    }
-  })
 })
-
 
